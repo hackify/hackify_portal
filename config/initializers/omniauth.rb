3dd8de3b4de1c8bb9e9d9056f004c83adbc50a33
Michael Dausmann <mdausmann@gmail.com>
2014-02-19 20:39:52 +1100

    Google analytics and application config fixes #4

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,5 @@
 #OmniAuth.config.logger = Rail.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :developer unless Rails.env.production?
-  #provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  if Rails.env.production?
-    provider :github, '896a6e1147188379abd0', 'ced9d1d6b3c5ed960ec4a179ec5455ec0128e28a'
-  else
-    provider :github, '5e3cd15f7140278106d7', '2efd62e1d5faa4a6a3231d734ce375ca54656bfa'
-  end
-
+  provider :github, CONFIG[:twitter_key], CONFIG[:twitter_secret]
 end
